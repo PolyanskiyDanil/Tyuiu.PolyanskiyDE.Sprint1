56366222a975a34e6094a44cf73b9dd7d070de12
Tyuiu.PolyanskiyDE <danilp.2005@mail.ru>
2023-09-22 16:39:41 +0500

    Task0

---
+++ Tyuiu.PolyanskiyDE.Sprint1.Task0.V16.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PolyanskiyDE.Sprint1.Task0.V16.Lib;
+
+namespace Tyuiu.PolyanskiyDE.Sprint1.Task0.V16.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExprission()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-7, res);
+        }
+    }
+}
---
+++ Tyuiu.PolyanskiyDE.Sprint1.Task0.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PolyanskiyDE.Sprint1.Task0.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PolyanskiyDE.Sprint1.Task0.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("916041c8-0121-40f0-9c17-f1f7cf48a713")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
